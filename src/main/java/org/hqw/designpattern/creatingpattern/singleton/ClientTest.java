e38f89b0fc41ebdcd4c49cfb335a894d099b217d
huqiwei <huqiwei@longshine.com>
2020-07-17 12:04:27 +0800

    (singleton)add lazy singleton

--- src/main/java/org/hqw/designpattern/creatingpattern/singleton/ClientTest.java
+++ src/main/java/org/hqw/designpattern/creatingpattern/singleton/ClientTest.java
@@ -3,13 +3,20 @@ package org.hqw.designpattern.creatingpattern.singleton;
 public class ClientTest {
     public static void main(String[] args) {
         /* 饿汉模式测试 */
-        System.out.println("打印第一次的Sun对象:" + HungrySingleton.getSun());
-        System.out.println("打印第二次的Sun对象:" + HungrySingleton.getSun());
+        System.out.println("饿汉模式打印第一次的Sun对象:" + HungrySingleton.getSun());
+        System.out.println("饿汉模式打印第二次的Sun对象:" + HungrySingleton.getSun());
         /**
-         * org.hqw.designpattern.creatingpattern.singleton.Sun@1b6d3586
-         * org.hqw.designpattern.creatingpattern.singleton.Sun@1b6d3586
+         * 饿汉模式打印第一次的Sun对象:org.hqw.designpattern.creatingpattern.singleton.Sun@1b6d3586
+         * 饿汉模式打印第二次的Sun对象:org.hqw.designpattern.creatingpattern.singleton.Sun@1b6d3586
          */
 
-        /* 饿汉模式测试 */
+        /* 懒汉模式测试 */
+        System.out.println("懒汉模式打印第一次的Sun对象:" + LazySingleTon.getSun());
+        System.out.println("懒汉模式打印第二次的Sun对象:" + LazySingleTon.getSun());
+        /**
+         * 懒汉模式打印第一次的Sun对象:org.hqw.designpattern.creatingpattern.singleton.Sun@4554617c
+         * 懒汉模式打印第二次的Sun对象:org.hqw.designpattern.creatingpattern.singleton.Sun@4554617c
+         */
+
     }
 }
--- src/main/java/org/hqw/designpattern/creatingpattern/singleton/LazySingleTon.java
+++ src/main/java/org/hqw/designpattern/creatingpattern/singleton/LazySingleTon.java
@@ -2,7 +2,21 @@ package org.hqw.designpattern.creatingpattern.singleton;
 
 /**
  * 懒汉模式
- * 需要的时候才去实例化其包含的类
+ * 1.需要的时候才去实例化其包含的类
+ * 2.多线程需要添加volatile和synchronized，保证懒汉模式的线程安全，否则线程不安全，会创建多个不同的实例
  */
 public class LazySingleTon {
+    /* volatile保证多线程数据一致性 */
+    private static volatile Sun sun = null;
+
+    private LazySingleTon(){}
+
+    /* synchronized保证只创建一个Sun实例 */
+    public static synchronized Sun getSun(){
+        if(sun == null){
+            sun = new Sun("circle");
+        }
+
+        return sun;
+    }
 }
